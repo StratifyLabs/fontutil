abc000ecfcf857440079117cc47b39a932b6c5b1
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2017-10-19 14:53:23 -0600

    Ported for stratify graphics (sbf stratify bitmap font files)

---
+++ src/BmpFont.cpp
@@ -0,0 +1,18 @@
+/*
+ * BmpFont.cpp
+ *
+ *  Created on: Oct 19, 2017
+ *      Author: tgil
+ */
+
+#include "BmpFont.hpp"
+
+BmpFont::BmpFont() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BmpFont::~BmpFont() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/BmpFont.hpp
@@ -0,0 +1,17 @@
+/*
+ * BmpFont.hpp
+ *
+ *  Created on: Oct 19, 2017
+ *      Author: tgil
+ */
+
+#ifndef BMPFONT_HPP_
+#define BMPFONT_HPP_
+
+class BmpFont {
+public:
+	BmpFont();
+	virtual ~BmpFont();
+};
+
+#endif /* BMPFONT_HPP_ */
